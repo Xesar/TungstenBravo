4b23f30545d947ddda1dc5e52e94fa636d85964f
Xesar <fzakowicz@op.pl>
2020-03-03 12:41:34 +0100

    feel classy today, might scrap later, idk

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
-namespace TungstenBravo
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+class rpn{
+    public rpn(string formula){
+
     }
+    private bool checkFormula(string formula){
+
+    }
+    //string?
+    private string[] getTokenList(){
+
+    }
+    //string?
+    private string[] convertToRpn(string formula){
+
+    }
+    private double calculateX(double x){
+
+    }
+    private double[] calculateBetween(int n, double xMin, double xMax){
+        
+    }
+}
+
+class Program{
+	static void Main(string[] args){
+		Console.WriteLine("I'm still empty o/");
+	}
 }
