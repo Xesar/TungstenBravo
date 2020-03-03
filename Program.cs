c1fa3d3fc7b5d096c0132af9a6ecb9e35e0110c3
Xesar <fzakowicz@op.pl>
2020-03-03 14:02:27 +0100

    Cleaned

--- Program.cs
+++ Program.cs
@@ -1,28 +1,5 @@
 ﻿using System;
 
-class rpn{
-    public rpn(string formula){
-
-    }
-    private bool checkFormula(string formula){
-
-    }
-    //string?
-    private string[] getTokenList(){
-
-    }
-    //string?
-    private string[] convertToRpn(string formula){
-
-    }
-    private double calculateX(double x){
-
-    }
-    private double[] calculateBetween(int n, double xMin, double xMax){
-        
-    }
-}
-
 class Program{
 	static void Main(string[] args){
 		Console.WriteLine("I'm still empty o/");
