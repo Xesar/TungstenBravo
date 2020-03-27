a0facf20ff4ea9b3055f7c218affd266ac8f76c0
Xesar <fzakowicz@op.pl>
2020-03-27 01:15:17 +0100

    added further validation and negative numbers handling

--- rpn.cs
+++ rpn.cs
@@ -7,6 +7,10 @@ using System.Text.RegularExpressions;
 class RPN{
 	private string formula;
 	private string errorMsg;
+	private bool preValidated = false;
+	private bool tokensDivided = false;
+	private bool postValidated = false;
+	private bool postfixParsed = false;
 	private List<string> infixTokens = new List<string>();
 	private List<string> postfixTokens = new List<string>();
 	private static Dictionary<string,int> d = new Dictionary<string,int>(){
@@ -18,10 +22,60 @@ class RPN{
 		{"(",0}
 	};
 	public RPN(string formula){
-		this.formula = Regex.Replace(formula,@"\s+","").ToLower();
-		this.formula = this.formula.Replace(".",",");
+		this.formula=Regex.Replace(formula,@"\s+","").ToLower();
+		this.formula=this.formula.Replace(".",",");
+		if(this.formula[0]=='-') this.formula=this.formula.Insert(0,"0");
+	}
+	public bool preValidate(){
+		Regex operators = new Regex(@"[\+*/^%]");
+		int bracketCount = 0;
+
+		if(string.IsNullOrEmpty(formula)){
+			errorMsg="No formula detected";
+			return false;
+		}
+
+		for(int i=0; i<formula.Length; i++){
+			if(formula[i]=='('){
+				if(formula[i+1]==')'){
+					errorMsg="Empty brackets";
+					return false;
+				}
+				bracketCount++;
+			}
+			if(formula[i]==')') bracketCount--;
+		}
+		if(bracketCount!=0){
+			errorMsg="Wrong number of brackets";
+			return false;
+		}
+
+		string temp = operators.Replace(formula,".");
+		string[] incorrectOperatorOrder = new string[]{"(.)","(.",".)","..",",,","(,",",)","(-)","-)","--"};
+		foreach(string str in incorrectOperatorOrder){
+			if(temp.Contains(str)){
+				errorMsg="Wrong operator order";
+				return false;
+			}
+		}
+
+		if(formula.StartsWith("*") || formula.StartsWith("/") || formula.StartsWith("+") || formula.StartsWith("-") || formula.StartsWith("^")){
+			errorMsg="Cannot begin formula with an operator";
+			return false;
+		}
+
+		if(formula.EndsWith("*") || formula.EndsWith("/") || formula.EndsWith("+") || formula.EndsWith("-") || formula.EndsWith("^")){
+			errorMsg="Cannot end formula with an operator";
+			return false;
+		}
+		this.preValidated=true;
+		return true;
 	}
 	public void divideTokens(){
+		if(!preValidated){
+			Console.WriteLine("Pre-validate first");
+			return;
+		}
 		string shortTokens = "abs cos exp log sin tan";
 		string longTokens = "sqrt cosh sinh tanh acos asin atan";
 		for(int i=0; i<formula.Length; i++){
@@ -34,6 +88,8 @@ class RPN{
 				longToken = formula[i].ToString()+formula[i+1].ToString()+formula[i+2].ToString()+formula[i+3].ToString();
 			}
 			if("()^*/+-".Contains(formula[i])){
+				if(i>0 && formula[i-1]=='(' && formula[i]=='-')
+					infixTokens.Add(0d.ToString());
 				infixTokens.Add(formula[i].ToString());
 			}else if(shortToken.Length>0 && shortTokens.Contains(shortToken)){
 				infixTokens.Add(shortToken);
@@ -42,16 +98,46 @@ class RPN{
 				infixTokens.Add(longToken);
 				i+=3;
 			}else{
+				if(i+1<formula.Length && formula[i]=='x' && "1234567890,".Contains(formula[i+1])){
+					infixTokens.Add("x");
+					infixTokens.Add("*");
+					i++;
+				}
 				string temp = formula[i].ToString();
 				while(i+1<formula.Length && "1234567890,".Contains(formula[i+1])){
 					temp+=formula[i+1].ToString();
 					i++;
 				}
 				infixTokens.Add(temp);
+				if(i+1<formula.Length && formula[i+1]=='x')
+					infixTokens.Add("*");
 			}
 		}
+		this.tokensDivided=true;
+	}
+	public bool postValidate(){
+		if(!tokensDivided){
+			Console.WriteLine("Divide tokens first");
+			return false;
+		}
+		Regex numRegex = new Regex(@"^[0-9]+$");
+		string[] allowedTokens = new string[]{"(",")","+","-","/","*","^","x","abs","cos","exp","log","sin","tan","sqrt","cosh","sinh","tanh","acos","asin","atan"};
+		
+		foreach(string token in infixTokens){
+			if(!numRegex.IsMatch(token) && !allowedTokens.Contains(token)){
+				Console.WriteLine(token+": not number/operator/math function or other unallowed character");
+				return false;
+			}
+		}
+		
+		this.postValidated=true;
+		return true;
 	}
 	public void toPostfix(){
+		if(!postValidated){
+			Console.WriteLine("Post-validate first");
+			return;
+		}
 		Stack<string> s = new Stack<string>();
 		for(int i=0; i<infixTokens.Count; i++){
 			if(!("()^*/+-".Contains(infixTokens[i])) && !(d.ContainsKey(infixTokens[i])))
@@ -70,52 +156,13 @@ class RPN{
 		}
 		while(s.Count>0)
 			postfixTokens.Add(s.Pop());
-	}
-	public bool validate(){
-		Regex operators = new Regex(@"[\-+*/^%]");
-		int bracketCount = 0;
-
-		for(int i=0; i<formula.Length; i++){
-			if(formula[i]=='('){
-				if(formula[i+1]==')'){
-					errorMsg="Epmty brackets";
-					return false;
-				}
-				bracketCount++;
-			}
-			if(formula[i]==')') bracketCount--;
-		}
-		if(bracketCount!=0){
-			errorMsg="Wrong number of brackets";
-			return false;
-		}
-
-		if(string.IsNullOrEmpty(formula)){
-			errorMsg="No formula detected";
-			return false;
-		}
-
-		string temp = operators.Replace(formula,".");
-		string[] incorrectOperatorOrder = new string[]{"(.",".)","..",",,","(,",",)"};
-		foreach(string str in incorrectOperatorOrder){
-			if(temp.Contains(str)){
-				errorMsg="Wrong operator order";
-				return false;
-			}
-		}
-
-		if(formula.StartsWith("*") || formula.StartsWith("/") || formula.StartsWith("+") || formula.StartsWith("-") || formula.StartsWith("^")){
-			errorMsg="Cannot begin formula with an operator";
-			return false;
-		}
-
-		if(formula.EndsWith("*") || formula.EndsWith("/") || formula.EndsWith("+") || formula.EndsWith("-") || formula.EndsWith("^")){
-			errorMsg="Cannot end formula with an operator";
-			return false;
-		}
-		return true;
+		this.postfixParsed=true;
 	}
 	public double evaluateForX(double x){
+		if(!postfixParsed){
+			Console.WriteLine("Parse to postfix fisrt");
+			return double.NaN;
+		}
 		Stack<string> s = new Stack<string>();
 		Regex numRegex = new Regex(@"^[0-9]+$");
 		string mathFunctions = "abs cos exp log sin tan sqrt cosh sinh tanh acos asin atan";
