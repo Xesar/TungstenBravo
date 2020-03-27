09eb84941d03441f74af22ec2bdccdd2fe8dbaa4
Xesar <fzakowicz@op.pl>
2020-03-27 01:08:03 +0100

    added further validation and negative numbers handling

--- Program.cs
+++ Program.cs
@@ -2,6 +2,25 @@
 
 class Program{
 	static void Main(string[] args){
-        Console.WriteLine("Empty again!");
+        // RPN rpn = new RPN("x");
+        RPN rpn = new RPN("-x2+(-2)+c");
+        // RPN rpn = new RPN("sin(x/2)");
+        rpn.printFormula();
+        if(rpn.preValidate()){
+            rpn.divideTokens();
+            if(rpn.postValidate()){
+                rpn.printInfix();
+                rpn.toPostfix();
+                rpn.printPostfix();
+                Console.WriteLine(rpn.evaluateForX(1));
+            }
+            // double[,] results = rpn.evaluateForRange(2,6,5);
+            // for(int i=0; i<results.GetLength(1);i++){
+            //     Console.WriteLine(results[0,i]+":\t"+results[1,i]);
+            // }
+        }else{
+            Console.WriteLine("Hey boss, problem:");
+            Console.WriteLine(rpn.getErrorMsg().ToLower()+"");
+        }
 	}
 }
